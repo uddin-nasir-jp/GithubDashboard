41bf773c047129397840f20b5abb6baa9331f9d6
Nasir Uddin <uddin.nasir.jp@gmail.com>
2024-12-17 18:02:53 +0900

    Initial Commit

---
+++ GithubDashboard/GithubDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDashboardApp.swift
+//  GithubDashboard
+//
+//  Created by Nasir Uddin on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
